72ab5145288193a2827742d05b99a99625cf4946
John Boja <crisostomoiboja@gmail.com>
2024-05-20 07:35:36 +0800

    addition of home timer and welcome message hehe

--- HOAWinApp/UserControls/Home.Designer.cs
+++ HOAWinApp/UserControls/Home.Designer.cs
@@ -29,17 +29,41 @@
         private void InitializeComponent()
         {
             this.homeRightPanel = new System.Windows.Forms.Panel();
+            this.timeDateLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.homeRightPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // homeRightPanel
             // 
             this.homeRightPanel.BackColor = System.Drawing.Color.Transparent;
             this.homeRightPanel.BackgroundImage = global::HOAWinApp.Properties.Resources.db3__2_2;
+            this.homeRightPanel.Controls.Add(this.label1);
+            this.homeRightPanel.Controls.Add(this.timeDateLabel);
             this.homeRightPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.homeRightPanel.Location = new System.Drawing.Point(0, 0);
             this.homeRightPanel.Name = "homeRightPanel";
             this.homeRightPanel.Size = new System.Drawing.Size(984, 421);
             this.homeRightPanel.TabIndex = 1;
+            // 
+            // timeDateLabel
+            // 
+            this.timeDateLabel.AutoSize = true;
+            this.timeDateLabel.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeDateLabel.Location = new System.Drawing.Point(204, 229);
+            this.timeDateLabel.Name = "timeDateLabel";
+            this.timeDateLabel.Size = new System.Drawing.Size(0, 64);
+            this.timeDateLabel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rubik Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(316, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WELCOME";
             // 
             // Home
             // 
@@ -48,11 +72,15 @@
             this.Controls.Add(this.homeRightPanel);
             this.Name = "Home";
             this.Size = new System.Drawing.Size(984, 421);
+            this.homeRightPanel.ResumeLayout(false);
+            this.homeRightPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel homeRightPanel;
+        private System.Windows.Forms.Label timeDateLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
--- HOAWinApp/UserControls/Home.cs
+++ HOAWinApp/UserControls/Home.cs
@@ -7,16 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf;
 using MySql.Data.MySqlClient;
 
 namespace HOAWinApp.UserControls
 {
     public partial class Home : UserControl
     {
+        private Timer timer;
 
         public Home()
         {
             InitializeComponent();
+            InitializeTimer();
+            timeDateLabel.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         }
+        private void InitializeTimer()
+        {
+            // Create a Timer with interval set to 1 minute (60,000 milliseconds)
+            timer = new Timer();
+            timer.Interval = 1000; // 1 SEC
+            timer.Tick += Timer_Tick;
+
+            // Start the timer
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update the label's text with the current time and date
+            timeDateLabel.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+
     }
 }
